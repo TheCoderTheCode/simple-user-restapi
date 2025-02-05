7f6b9db51306b80f312fdf9c8a4300e8bac22cb8
Evandro Teixeira <evandro.development@gmail.com>
2025-02-05 17:21:37 -0300

    feat: add index files to folder to allow single source of truth

---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export { default as IUser } from "./IUser.types"
+export { default as IUserRepository } from "./IUserRepository.types"
+export { default as IUserService } from "./IUserService.types"
