4dca2417b460e7aec9e3f782b495041d53438707
Evandro Teixeira <evandro.development@gmail.com>
2025-02-08 11:50:40 -0300

    feat: update routes to use validation middleware

--- src/router/v1/userRoutes.ts
+++ src/router/v1/userRoutes.ts
@@ -1,13 +1,23 @@
 import { Router } from "express"
 import { UserController } from "@/controllers"
+import { validate } from "@/middleware"
+import { userSchema } from "@/schemas"
 
 const router = Router()
 
 const routes = (userController: UserController) => {
   router.get("/users", userController.getAllUsers.bind(userController))
   router.get("/users/:id", userController.getUserById.bind(userController))
-  router.post("/users", userController.createUser.bind(userController))
-  router.patch("/users/:id", userController.updateUser.bind(userController))
+  router.post(
+    "/users",
+    validate(userSchema),
+    userController.createUser.bind(userController),
+  )
+  router.patch(
+    "/users/:id",
+    validate(userSchema),
+    userController.updateUser.bind(userController),
+  )
   router.delete("/users/:id", userController.deleteUser.bind(userController))
 
   return router
